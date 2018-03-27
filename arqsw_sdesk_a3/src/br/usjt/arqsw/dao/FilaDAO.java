026123fc0231f009a2ee27e4317b9531b1fc4233
AccelEight <accelrocha@gmail.com>
2018-03-27 00:36:12 -0300

    Aula03

---
+++ arqsw_sdesk_a3/src/br/usjt/arqsw/dao/FilaDAO.java
@@ -0,0 +1,41 @@
+package br.usjt.arqsw.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import br.usjt.arqsw.entity.Fila;
+/**
+ * 
+ * @author Accel Gustavo Araujo Rocha	RA 81616654	SI3AN-MCA
+ *
+ */
+@Repository
+public class FilaDAO {
+	@PersistenceContext
+	EntityManager manager;
+
+	@SuppressWarnings("unchecked")
+	public List<Fila> listarFilas(){
+		return manager.createQuery("select f from Fila f").getResultList();
+	}
+
+	public void atualizar(Fila fila) {
+		manager.merge(fila);
+	}
+
+	public void remover(Fila fila) {
+		manager.remove(fila);
+	}
+
+	public Fila selecionar(int id) {
+		return manager.find(Fila.class, id);
+	}
+
+	public void cadastrarChamado(Fila fila){
+		manager.persist(fila);
+	}
+}
